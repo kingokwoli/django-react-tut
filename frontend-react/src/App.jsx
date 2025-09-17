7724f7b32590c4fbc13095b5d6451c5be262fd87
King <kingokwoli@gmail.com>
2025-09-17 07:51:03 +0100

    homepage react

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,10 +1,16 @@
-
+import './assets/css/style.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Main from './components/Main'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <></>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   )
 }
 
---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer py-3 my-3">
+        <hr className="border-bottom" />
+        <p className="text-light text-center">
+          &copy; 2025 - built with ❤️ by KO
+        </p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
