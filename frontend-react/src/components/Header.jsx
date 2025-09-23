4092f2ebb2d27b0f33f4db7262c453b0a9549a57
King <kingokwoli@gmail.com>
2025-09-23 17:35:44 +0100

    Login functionality

--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -1,17 +1,36 @@
-import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { Link, useNavigate } from "react-router-dom"
 import Button from "./Button"
+import { AuthContext } from "../AuthProvider"
 
 
 const Header = () => {
+    const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext)
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        localStorage.removeItem('accessToken')
+        localStorage.removeItem('refreshToken')
+        setIsLoggedIn(false)
+        console.log('Logged out successfully!')
+        navigate('/login')
+    }
+    
     return (
         <>
             <nav className="navbar container pt-3 pb-3 align-items-start">
                 <Link className="navbar-brand text-light" to='/'>Practice Portal Site</Link>
 
                 <div>
-                    <Button text='Login' class="btn-outline-info" url="/login" />
-                    &nbsp;
-                    <Button text='Register' class="btn-info" url="/register" />
+                    {isLoggedIn ? (
+                        <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
+                    ) : (
+                            <>
+                                <Button text='Login' class="btn-outline-info" url="/login" />
+                                &nbsp;
+                                <Button text='Register' class="btn-info" url="/register" />
+                            </>
+                    )}
                 </div>
             </nav>
         </>
